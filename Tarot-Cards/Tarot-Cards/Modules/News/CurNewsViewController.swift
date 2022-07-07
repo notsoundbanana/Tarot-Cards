fd18c4e901962418e8018a6cfca85c1454eeaec2
polinom <pryftina@gmail.com>
2022-07-07 18:39:18 +0400

    add News model

---
+++ Tarot-Cards/Tarot-Cards/Modules/News/CurNewsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CurNewsViewController.swift
+//  Tarot-Cards
+//
+//  Created by Полина Рыфтина on 07.07.2022.
+//
+
+import UIKit
+
+class CurNewsViewController: UIViewController {
+
+    // MARK: - View life cyrcle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Tarot-Cards/Tarot-Cards/Modules/News/NewsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  NewsTableViewCell.swift
+//  Tarot-Cards
+//
+//  Created by Полина Рыфтина on 05.07.2022.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    // MARK: - UI
+    
+    @IBOutlet weak var newsView: UIView!
+    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
