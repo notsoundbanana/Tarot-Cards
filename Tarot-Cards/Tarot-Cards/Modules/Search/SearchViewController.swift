0b87b13f3653c910fcba0043e51d8c55a3ead770
Danil Chemaev <45203038+notsoundbanana@users.noreply.github.com>
2022-07-07 17:40:53 +0300

    Update SearchViewController.swift

--- Tarot-Cards/Tarot-Cards/Modules/Search/SearchViewController.swift
+++ Tarot-Cards/Tarot-Cards/Modules/Search/SearchViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SearchViewController: UIViewController{
+class SearchViewController: UIViewController {
       
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
