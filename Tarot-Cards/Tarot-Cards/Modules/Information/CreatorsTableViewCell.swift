70f750909f74a2b409242afbe92803a7b749c13f
Gusarowa <elengusarowa@yandex.ru>
2022-07-08 16:28:26 +0300

    AboutPage is partly done

---
+++ Tarot-Cards/Tarot-Cards/Modules/Information/CreatorsTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  CreatorsTableViewCell.swift
+//  Tarot-Cards
+//
+//  Created by Лена Гусарова on 07.07.2022.
+//
+
+import UIKit
+
+class CreatorsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
---
+++ Tarot-Cards/Tarot-Cards/Modules/Information/CreatorsTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  CreatorsTableViewController.swift
+//  Tarot-Cards
+//
+//  Created by Лена Гусарова on 07.07.2022.
+//
+
+struct Creators{
+    let name: String
+}
+
+
+import UIKit
+
+class CreatorsTableViewController: UITableViewController {
+
+    let creators : [Creators] = [
+        Creators(name: "Polina"),
+        Creators(name: "Danil"),
+        Creators(name: "Artem"),
+        Creators(name: "Elena"),
+        Creators(name: "Andrey")
+        ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return creators.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CreatorsTableViewCell", for: indexPath) as? CreatorsTableViewCell else {return UITableViewCell()}
+        
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+        90
+    }
+    
+    
+    
+    
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
